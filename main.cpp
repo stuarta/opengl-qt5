11a8da526cf2f1f5122e4f2b071c220906a7ab5f
Stuart Auchterlonie <stuarta@mythtv.org>
2017-08-31 23:19:00 +0100

    Initial Version, the triangle lives!

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QGuiApplication>
+#include "window.h"
+ 
+int main(int argc, char *argv[])
+{
+  QGuiApplication app(argc, argv);
+ 
+  // Set OpenGL Version information
+  // Note: This format must be set before show() is called.
+  QSurfaceFormat format;
+  format.setRenderableType(QSurfaceFormat::OpenGL);
+ 
+  // Set the window up
+  Window window;
+  window.setFormat(format);
+  window.resize(QSize(800, 600));
+  window.show();
+ 
+  return app.exec();
+}
---
+++ window.cpp
@@ -0,0 +1,43 @@
+#include "window.h"
+
+#include <QPainter>
+#include <QMatrix4x4>
+#include <QOpenGLFunctions>
+
+#include "shaders.h"
+
+Window::Window()
+{
+}
+
+void Window::initializeGL()
+{
+    initializeOpenGLFunctions();
+    m_program = new QOpenGLShaderProgram(this);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vs_source);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fs_source);
+    m_program->link();
+    m_attribute_coord2d = m_program->attributeLocation("coord2d");
+}
+
+void Window::resizeGL(int width, int height)
+{
+  // Currently we are not handling width/height changes
+  (void)width;
+  (void)height;
+}
+
+void Window::paintGL()
+{
+//    glClearColor(.4f, .7f, .1f, 0.5f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    m_program->bind();
+    glVertexAttribPointer(m_attribute_coord2d, 2, GL_FLOAT, GL_FALSE, 0, triangle_vertices);
+    glEnableVertexAttribArray(m_attribute_coord2d);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(m_attribute_coord2d);
+    m_program->release();
+}
+
