4ecdbee06c12fe14c196717aaa8482136dadc8f4
Stuart Auchterlonie <stuarta@mythtv.org>
2017-09-01 12:45:30 +0100

    Fix indentation to be consistent

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
  
 int main(int argc, char *argv[])
 {
-  QGuiApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
  
-  // Set OpenGL Version information
-  // Note: This format must be set before show() is called.
-  QSurfaceFormat format;
-  format.setRenderableType(QSurfaceFormat::OpenGL);
+    // Set OpenGL Version information
+    // Note: This format must be set before show() is called.
+    QSurfaceFormat format;
+    format.setRenderableType(QSurfaceFormat::OpenGL);
  
-  // Set the window up
-  Window window;
-  window.setFormat(format);
-  window.resize(QSize(800, 600));
-  window.show();
+    // Set the window up
+    Window window;
+    window.setFormat(format);
+    window.resize(QSize(800, 600));
+    window.show();
  
-  return app.exec();
+    return app.exec();
 }
