d3dc9770b99503cd7f4686368a8e46ab42c137ed
Stuart Auchterlonie <stuarta@mythtv.org>
2017-10-04 23:28:08 +0100

    OpenGLES wants the precision in the fragment shader

--- include/shaders03.h
+++ include/shaders03.h
@@ -14,7 +14,7 @@ const char *vs_source =
 const char *fs_source =
     //"#version 100\n"  // OpenGL ES 2.0
     //"#version 120\n"  // OpenGL 2.1
-    "varying vec3 f_color;    "
+    "varying highp vec3 f_color;    "
     "void main(void) {        "
     "  gl_FragColor = vec4(f_color.r, f_color.g, f_color.b, 1.0); "
     "}";
