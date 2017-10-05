7425249d542c7db9778f8c15868236865951b69e
Stuart Auchterlonie <stuarta@mythtv.org>
2017-10-05 12:05:21 +0100

    Sample code that opens VAAPI via wayland display

---
+++ 01e-with-vaapi-wayland/main.cpp
@@ -0,0 +1,59 @@
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QX11Info>
+
+#include "window.h"
+
+#include <va/va.h>
+#include <va/va_glx.h>
+#include <va/va_wayland.h>
+
+#include <wayland-client.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    // QDesktopWidget allows us to get the multi screen data
+    QDesktopWidget  *m_desktop = app.desktop();
+    // Connect to wayland
+    struct wl_display *m_wl_display = wl_display_connect(NULL);
+    // libva get wayland context for vaapi
+    VADisplay       m_va_display = vaGetDisplayWl(m_wl_display);
+
+    // Init VAAPI
+    VAStatus va_status;
+    int major_ver, minor_ver;
+    va_status = vaInitialize(m_va_display, &major_ver, &minor_ver);
+    if (va_status != VA_STATUS_SUCCESS)
+    {
+        qInfo() << vaErrorStr(va_status);
+    }
+    qInfo() << "Is VA display valid? " << vaDisplayIsValid(m_va_display);
+    qInfo() << "VAAPI Vendor = " << vaQueryVendorString(m_va_display);
+
+    // Set OpenGL Version information
+    // Note: This format must be set before show() is called.
+    QSurfaceFormat format;
+    format.setRenderableType(QSurfaceFormat::OpenGL);
+
+    // Basic multi screen information
+    qInfo() << "Is Virtual Desktop?" << m_desktop->isVirtualDesktop();
+    qInfo() << "System has" << m_desktop->screenCount() << "screens, v_width"
+            << m_desktop->width() << ",v_height" << m_desktop->height();
+    for (int i = 0; i < m_desktop->screenCount(); i++) {
+        qInfo() << "Geometry:" << m_desktop->screenGeometry(i);
+    }
+
+    // Pick a random screen from those available
+    int m_targetscreen = rand() % m_desktop->screenCount();
+
+    // Set the window up
+    mWindow window(m_desktop->screen(m_targetscreen));
+    window.setFormat(format);
+    window.resize(QSize(800, 600));
+    //Qt::WindowFullScreen works with QOpenGLWidget, but not QOpenGLWindow
+    //window.setWindowState(Qt::WindowFullScreen);
+    window.show();
+
+    return app.exec();
+}
